271d156e5144d70f9341214cd0522048dfafea5c
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2023-01-10 21:35:41 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/BNLOS/android_device_xiaomi_plato -b 12"
 
-export DEVICE="beryllium"
+export DEVICE="plato"
 export OEM="xiaomi"
 
 # Build Target
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=Raaydown"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
